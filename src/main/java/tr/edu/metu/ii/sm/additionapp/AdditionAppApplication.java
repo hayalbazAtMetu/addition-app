e52726b8d7ada79646aecf502023d69c960ab27f
Şükrücan Taylan Işıkoğlu <taylan.isikoglu@metu.edu.tr>
2023-06-07 13:39:22 +0000

    Initial commit

---
+++ src/main/java/tr/edu/metu/ii/sm/additionapp/AdditionAppApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.metu.ii.sm.additionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditionAppApplication.class, args);
+	}
+
+}
